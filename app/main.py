78564d7b7c9aecfa03d00f2fd1b91eb7938cf839
trinesh987 <trinesh1438@gmail.com>
2024-10-29 23:34:18 +0530

    Tcp server implementation

--- app/main.py
+++ app/main.py
@@ -6,9 +6,24 @@ def main():
     print("Logs from your program will appear here!")
 
     
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() 
+    server_socket = socket.create_server(("localhost", 4221))
 
+    print("Tcp server listening on port 4221")
+
+    communication_socket,address = server_socket.accept()
+
+    print(f"connectd by {address}")
+
+    while True:
+        data = communication_socket.recv(1024)
+        if not data:
+            break
+        print(f"received data:{data.decode()}")
+
+        communication_socket.sendall(data)
+
+    communication_socket.close()
+    print("Connection closed")
 
 if __name__ == "__main__":
     main()
